476e38243891b74818657397d24acc594797203e
Diego <diego-jofre@live.com.ar>
2021-03-11 16:39:51 -0300

    validacion token con header seteado

--- back/middlewareAuthJWT/jwt.js
+++ back/middlewareAuthJWT/jwt.js
@@ -4,16 +4,20 @@ const checkJWT = (req, res, next) => {
   if (!req.headers.authorization) {
     return res.status(401).send("usuario inexistente");
   }
-
   const token = req.headers.authorization.split(" ")[1];
-  const data = jwt.verify(token, "ebook");
+
+  const data = jwt.verify(token, "ebook", function (err, decoded) {
+    if (err) {
+      res.status(401).send("no podes entrar");
+    }
+    return decoded
+  });
 
   if (data) {
     req.user = data;
-    next();
-  } else {
-    return res.status(401).send("no podes entrar");
+    return next();
   }
+
 };
 
 module.exports = checkJWT;
