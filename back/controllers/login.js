763d63e0618352b14093c4eefb7a552b2e1c7e05
Fabricio Aitta <alexanderaitta@gmail.com>
2021-03-11 15:54:36 -0300

    ultima modificacion jueves

--- back/controllers/login.js
+++ back/controllers/login.js
@@ -2,8 +2,12 @@ const User = require('../models/User');
 
 const loginController = {
     find(req, res){
-        User.findOne({nombre : req.body.nombre})
-        .then(user => res.send(user)) //ACA FALTA VALIDACION PORQUE SEA NULL O UN USER IGUAL ENTRA AL RES.SEND
+        User.findOne({email : req.body.email})
+        .then(user => {
+            
+            
+        
+        }) //ACA FALTA VALIDACION PORQUE SEA NULL O UN USER IGUAL ENTRA AL RES.SEND
         .catch(e => res.sendStatus(401))
     }
 }
--- back/routes/index.js
+++ back/routes/index.js
@@ -4,10 +4,12 @@ const productsRouter = require('./productsRouter')
 const registerRouter = require('./registerRouter')
 const loginRouter = require('./loginRouter')
 const categoriasRouter = require('./categoriasRouter')
+const homeRouter = require('./homeRouter')
 
 router.use("/products", productsRouter)
 router.use("/register", registerRouter)
 router.use("/login", loginRouter)
 router.use("/categorias", categoriasRouter)
+// router.use("/home", homeRouter)
 
 module.exports = router
